48b84aed1d5a916e4877c6cbeb8ac711121027b3
phuockaus <phuoc.doan0412@hcmut.edu.vn>
2021-11-05 09:15:37 +0700

    Add Preprocessing module

---
+++ Preprocessing.py
@@ -0,0 +1,31 @@
+from PreProcessing.VnmPreprocessing import VnmPreprocessing
+from PreProcessing.EngPreprocessing import EngPreprocessing
+from abc import ABC
+
+
+class Preprocessing(ABC):
+    @staticmethod
+    def VieCollectionProcessing(collection):
+        pass
+
+    @staticmethod
+    def EngCollectionProcessing(collection):
+        pass
+
+
+class WordPreprocessing(Preprocessing):
+    @staticmethod
+    def VieCollectionProcessing(collection):
+        return list(map(lambda item: VnmPreprocessing.preprocess2word(item['Content']), collection))
+
+    @staticmethod
+    def EngCollectionProcessing(collection):
+        return list(map(lambda item: EngPreprocessing.preprocess2word(item['Content']), collection))
+
+    @staticmethod
+    def VieFilesProcessing(files):
+        return list(map(lambda item: VnmPreprocessing.preprocess2word(item.getContent()), files))
+
+    @staticmethod
+    def EngFilesProcessing(files):
+        return list(map(lambda item: EngPreprocessing.preprocess2word(item.getContent()), files))
